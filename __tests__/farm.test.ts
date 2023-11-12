e2504c39a8c3da73303d1db70220694dd2418f6c
Daniel Garcia Briseno <daniel@dangarbri.tech>
2023-11-12 15:52:31 -0500

    Implement inventorying the farm

--- __tests__/farm.test.ts
+++ __tests__/farm.test.ts
@@ -4,7 +4,13 @@ describe('Farm', () => {
     it('Should be able to read the smart contract', async () => {
         const farm = new Farm(process.env.FARM_ADDRESS);
         expect(farm).toBeDefined();
-        const amount = await farm.Amount(210);
+        const amount = await farm.Amount("210");
         expect(amount).toBeGreaterThan(0);
+    });
+    it('Can inventory the farm', async () => {
+        const farm = new Farm(process.env.FARM_ADDRESS);
+        let inventory = await farm.Inventory();
+        expect(inventory).toBeDefined();
+        expect(inventory.length).toBeGreaterThan(0);
     });
 })
