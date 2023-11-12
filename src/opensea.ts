49c3eed3de77b2e2795382ed48bd5e10adcacac4
Daniel Garcia Briseno <daniel@dangarbri.tech>
2023-11-12 16:41:56 -0500

    GetPrice -> Price

--- src/opensea.ts
+++ src/opensea.ts
@@ -42,7 +42,7 @@ class Opensea {
         return nfts;
     }
 
-    static async GetPrice(address: string, id: number): Promise<number> {
+    static async Price(address: string, id: number): Promise<number> {
         const listing = await this.GetListings(address, id);
         if (listing['length'] > 0) {
             const bundlePrice = parseInt(listing['orders'][0]['current_price']);
